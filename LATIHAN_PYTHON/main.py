d6bc48a6403e21a5cf48ce213afe704861fc8b3d
Labib <111202315339@mhs.dinus.ac.id>
2025-03-12 00:13:25 +0700

    first commit

---
+++ LATIHAN_PYTHON/main.py
@@ -0,0 +1,10 @@
+#cara mengakses package yang dibuat, copy dan paste code dalam file main.py
+
+import os
+
+os.chdir(r"/0. labib/0. Mata Kuliah/Semester 4/Machine Learning/Minggu-2/Repositori 1")
+
+import latihan_package.alpha as a, latihan_package.beta as b
+
+a.alphaSatu()
+b.betaSatu()
